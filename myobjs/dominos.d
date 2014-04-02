219b52fe1e0498c68bd46dbafd4eec9daf36bb62
Pratyaksh <pratyaksh@me.com>
2014-04-02 20:02:10 +0530

    changed few box2d files

--- myobjs/dominos.d
+++ myobjs/dominos.d
@@ -1,18 +1,18 @@
-/home/sanket/project/git_repo/myobjs/dominos.o: \
- /home/sanket/project/git_repo/src/dominos.cpp \
- /home/sanket/project/git_repo/src/cs296_base.hpp \
- /home/sanket/project/git_repo/src/render.hpp \
- /home/sanket/project/git_repo/external/include/Box2D/Box2D.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2Settings.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h /usr/include/assert.h \
- /usr/include/features.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
- /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+/home/pratyaksh/296/cs296_base_code/myobjs/dominos.o: \
+ /home/pratyaksh/296/cs296_base_code/src/dominos.cpp \
+ /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /home/pratyaksh/296/cs296_base_code/src/cs296_base.hpp \
+ /home/pratyaksh/296/cs296_base_code/src/render.hpp \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Box2D.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2Settings.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stddef.h /usr/include/assert.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/float.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2Draw.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2Math.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/float.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2Draw.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2Math.h \
  /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/huge_val.h \
  /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
  /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
@@ -21,52 +21,53 @@
  /usr/include/x86_64-linux-gnu/bits/mathdef.h \
  /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
  /usr/include/x86_64-linux-gnu/bits/mathinline.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2Timer.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/Shapes/b2CircleShape.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/Shapes/b2Shape.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2BlockAllocator.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/b2Collision.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2Timer.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/Shapes/b2CircleShape.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/Shapes/b2Shape.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2BlockAllocator.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/b2Collision.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed/syslimits.h \
  /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
  /usr/include/x86_64-linux-gnu/bits/local_lim.h \
  /usr/include/linux/limits.h \
  /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
  /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/Shapes/b2ChainShape.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/b2BroadPhase.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/b2DynamicTree.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2GrowableStack.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/Shapes/b2ChainShape.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/b2BroadPhase.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/b2DynamicTree.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2GrowableStack.h \
  /usr/include/memory.h /usr/include/string.h /usr/include/xlocale.h \
  /usr/include/x86_64-linux-gnu/bits/string3.h \
- /usr/include/c++/4.6/algorithm /usr/include/c++/4.6/utility \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++config.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/os_defines.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/cpu_defines.h \
- /usr/include/c++/4.6/bits/stl_relops.h \
- /usr/include/c++/4.6/bits/stl_pair.h /usr/include/c++/4.6/bits/move.h \
- /usr/include/c++/4.6/bits/concept_check.h \
- /usr/include/c++/4.6/bits/stl_algobase.h \
- /usr/include/c++/4.6/bits/functexcept.h \
- /usr/include/c++/4.6/bits/exception_defines.h \
- /usr/include/c++/4.6/bits/cpp_type_traits.h \
- /usr/include/c++/4.6/ext/type_traits.h \
- /usr/include/c++/4.6/ext/numeric_traits.h \
- /usr/include/c++/4.6/bits/stl_iterator_base_types.h \
- /usr/include/c++/4.6/bits/stl_iterator_base_funcs.h \
- /usr/include/c++/4.6/bits/stl_iterator.h \
- /usr/include/c++/4.6/debug/debug.h /usr/include/c++/4.6/bits/stl_algo.h \
- /usr/include/c++/4.6/cstdlib /usr/include/stdlib.h \
- /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/c++/4.8/algorithm /usr/include/c++/4.8/utility \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/os_defines.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/cpu_defines.h \
+ /usr/include/c++/4.8/bits/stl_relops.h \
+ /usr/include/c++/4.8/bits/stl_pair.h /usr/include/c++/4.8/bits/move.h \
+ /usr/include/c++/4.8/bits/concept_check.h \
+ /usr/include/c++/4.8/bits/stl_algobase.h \
+ /usr/include/c++/4.8/bits/functexcept.h \
+ /usr/include/c++/4.8/bits/exception_defines.h \
+ /usr/include/c++/4.8/bits/cpp_type_traits.h \
+ /usr/include/c++/4.8/ext/type_traits.h \
+ /usr/include/c++/4.8/ext/numeric_traits.h \
+ /usr/include/c++/4.8/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.8/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.8/debug/debug.h \
+ /usr/include/c++/4.8/bits/stl_iterator.h \
+ /usr/include/c++/4.8/bits/stl_algo.h /usr/include/c++/4.8/cstdlib \
+ /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
  /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
  /usr/include/x86_64-linux-gnu/bits/endian.h \
  /usr/include/x86_64-linux-gnu/bits/byteswap.h \
- /usr/include/x86_64-linux-gnu/sys/types.h \
  /usr/include/x86_64-linux-gnu/bits/types.h \
- /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
  /usr/include/x86_64-linux-gnu/sys/select.h \
  /usr/include/x86_64-linux-gnu/bits/select.h \
  /usr/include/x86_64-linux-gnu/bits/sigset.h \
@@ -74,44 +75,50 @@
  /usr/include/x86_64-linux-gnu/bits/select2.h \
  /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
  /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
  /usr/include/x86_64-linux-gnu/bits/stdlib.h \
- /usr/include/c++/4.6/bits/algorithmfwd.h \
- /usr/include/c++/4.6/initializer_list \
- /usr/include/c++/4.6/bits/stl_heap.h \
- /usr/include/c++/4.6/bits/stl_tempbuf.h \
- /usr/include/c++/4.6/bits/stl_construct.h /usr/include/c++/4.6/new \
- /usr/include/c++/4.6/exception \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/b2Distance.h \
- /home/sanket/project/git_repo/external/include/Box2D/Collision/b2TimeOfImpact.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2Body.h \
- /usr/include/c++/4.6/memory /usr/include/c++/4.6/bits/allocator.h \
- /usr/include/c++/4.6/x86_64-linux-gnu/./bits/c++allocator.h \
- /usr/include/c++/4.6/ext/new_allocator.h \
- /usr/include/c++/4.6/bits/stl_uninitialized.h \
- /usr/include/c++/4.6/bits/stl_raw_storage_iter.h \
- /usr/include/c++/4.6/backward/auto_ptr.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2Fixture.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2WorldCallbacks.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2TimeStep.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2World.h \
- /home/sanket/project/git_repo/external/include/Box2D/Common/b2StackAllocator.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/b2ContactManager.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Contacts/b2Contact.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2Joint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2MotorJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
- /home/sanket/project/git_repo/external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
+ /usr/include/c++/4.8/bits/algorithmfwd.h \
+ /usr/include/c++/4.8/bits/stl_heap.h \
+ /usr/include/c++/4.8/bits/stl_tempbuf.h \
+ /usr/include/c++/4.8/bits/stl_construct.h /usr/include/c++/4.8/new \
+ /usr/include/c++/4.8/exception \
+ /usr/include/c++/4.8/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/4.8/ext/alloc_traits.h \
+ /usr/include/c++/4.8/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/4.8/bits/c++allocator.h \
+ /usr/include/c++/4.8/ext/new_allocator.h \
+ /usr/include/c++/4.8/bits/memoryfwd.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/b2Distance.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Collision/b2TimeOfImpact.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2Body.h \
+ /usr/include/c++/4.8/memory \
+ /usr/include/c++/4.8/bits/stl_uninitialized.h \
+ /usr/include/c++/4.8/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.8/backward/auto_ptr.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2Fixture.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2WorldCallbacks.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2TimeStep.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2World.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Common/b2StackAllocator.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/b2ContactManager.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Contacts/b2Contact.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2Joint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2MotorJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ /home/pratyaksh/296/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
  /usr/include/GL/freeglut.h /usr/include/GL/freeglut_std.h \
  /usr/include/GL/gl.h /usr/include/GL/glext.h /usr/include/inttypes.h \
- /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/GL/glu.h \
- /usr/include/GL/freeglut_ext.h /usr/include/c++/4.6/cstring \
- /home/sanket/project/git_repo/src/dominos.hpp
+ /usr/include/GL/freeglut_ext.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.8/include/stdarg.h \
+ /usr/include/c++/4.8/cstring \
+ /home/pratyaksh/296/cs296_base_code/src/dominos.hpp
